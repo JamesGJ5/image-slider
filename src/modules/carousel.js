49774f20e5fb902fbe5c5ce78194aaa3e621ac48
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-02 21:19:50 +0000

    Define three basic Carousel properties in its constructor

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,5 +1,15 @@
 export default class Carousel {
   constructor(carouselContainer) {
     this.carouselContainer = carouselContainer;
+    this.slideQuantity = this.countSlides();
+    this.currentImageIndex = this.initialiseCurrentImageIndex();
+  }
+
+  countSlides() {
+    return this.carouselContainer.querySelectorAll('.slide-list img').length;
+  }
+
+  initialiseCurrentImageIndex() {
+    return this.slideQuantity ? 0 : null;
   }
 }
