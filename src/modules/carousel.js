a36e8cb54f54c7393f97220347a80362bb0e5e45
James Graca-Jones <jamesgracajones@gmail.com>
2022-12-03 13:00:41 +0000

    Populate div.navigation upon instantiation of Carousel

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -3,6 +3,8 @@ export default class Carousel {
     this.carouselContainer = carouselContainer;
     this.slideQuantity = this.countSlides();
     this.currentImageIndex = this.initialiseCurrentImageIndex();
+
+    this.fillNavigationBar();
   }
 
   countSlides() {
@@ -11,5 +13,25 @@ export default class Carousel {
 
   initialiseCurrentImageIndex() {
     return this.slideQuantity ? 0 : null;
+  }
+
+  fillNavigationBar() {
+    const navigationBar = this.getNavigationBar();
+    for (let slideIndex = 0; slideIndex < this.slideQuantity; slideIndex += 1) {
+      navigationBar.appendChild(this.makeNavigationDot(slideIndex));
+    }
+  }
+
+  getNavigationBar() {
+    return this.carouselContainer.querySelector('.navigation');
+  }
+
+  makeNavigationDot(slideIndex) {
+    const navigationDot = document.createElement('button');
+    navigationDot.classList.add('dot');
+    if (slideIndex === 0) {
+      navigationDot.classList.add('active');
+    }
+    return navigationDot;
   }
 }
